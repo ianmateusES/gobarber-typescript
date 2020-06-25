c10aa19bd1742a0be33d66a17fbd6e9eeeb7e1cb
ianmateusES <ec.ianmateus@alu.ufc.br>
2020-06-25 02:00:19 -0300

    application backend start

---
+++ backend/src/routes/appointments.routes.ts
@@ -0,0 +1,37 @@
+import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+// http://localhost:3333/appointments
+const appointmentsRouter = Router();
+
+appointmentsRouter.get('/', async (req, res) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentsRepository.find();
+
+  return res.json(appointments);
+});
+
+appointmentsRouter.post('/', async (req, res) => {
+  try {
+    const { provider, date } = req.body;
+
+    const parseDate = parseISO(date);
+
+    const createAppointment = new CreateAppointmentService();
+
+    const appointment = await createAppointment.execute({
+      provider,
+      date: parseDate,
+    });
+
+    return res.json(appointment);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+});
+
+export default appointmentsRouter;
