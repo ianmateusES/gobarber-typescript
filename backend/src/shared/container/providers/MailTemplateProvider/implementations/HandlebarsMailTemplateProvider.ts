95c2c49044b0f098805b66cf8564b821f5a760ee
ianmateusES <ec.ianmateus@alu.ufc.br>
2020-09-25 10:08:03 -0300

    Template Mail

---
+++ backend/src/shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider.ts
@@ -0,0 +1,14 @@
+import handlebars from 'handlebars';
+
+import IMailTemplatesProvider from '../models/IMailTemplateProvider';
+import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO'
+
+class HandlebarsMailTemplatesProvider implements IMailTemplatesProvider {
+  public async parse({ template, variables }: IParseMailTemplateDTO): Promise<string> {
+    const parseTemplate = handlebars.compile(template);
+
+    return parseTemplate(variables);
+  }
+}
+
+export default HandlebarsMailTemplatesProvider;
