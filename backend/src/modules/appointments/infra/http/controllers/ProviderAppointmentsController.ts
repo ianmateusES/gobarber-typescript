66208af887a694d42c237eee43e28c694f1a2751
ianmateusES <ec.ianmateus@alu.ufc.br>
2021-02-17 17:41:28 -0300

    chore: API adjustment

--- backend/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ backend/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -2,11 +2,12 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
+import { classToClass } from 'class-transformer';
 
 export default class ProviderAppointmentsController {
   public async index(req: Request, res: Response): Promise<Response> {
     const provider_id = req.user.id;
-    const { day, month, year } = req.body;
+    const { day, month, year } = req.query;
 
     const listProviderAppointmentsService = container.resolve(
       ListProviderAppointmentsService,
@@ -14,11 +15,11 @@ export default class ProviderAppointmentsController {
 
     const appointments = await listProviderAppointmentsService.execute({
       provider_id,
-      day,
-      month,
-      year,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year),
     });
 
-    return res.json(appointments);
+    return res.json(classToClass(appointments));
   }
 }
--- backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -2,6 +2,7 @@ import { injectable, inject } from 'tsyringe';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
+import { classToClass } from 'class-transformer';
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -43,7 +44,7 @@ class ListProviderAppointmentsService {
         },
       );
 
-      await this.cacheProvider.save(cachekey, appointments);
+      await this.cacheProvider.save(cachekey, classToClass(appointments));
     }
 
     return appointments;
